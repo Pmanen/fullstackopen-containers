e835c8a4e5856eadd2abe02b711a235ffcdf25ec
Pippa van Manen <pippavanmanen@icloud.com>
2026-06-01 18:03:31 +0200

    ex 19

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -4,4 +4,11 @@ const apiClient = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
 })
 
+apiClient.interceptors.request.use((config) => {
+  if (config.url?.startsWith('/')) {
+    config.url = config.url.slice(1)
+  }
+  return config
+})
+
 export default apiClient
